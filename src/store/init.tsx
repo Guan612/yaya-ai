abd26b10efb703b2c78b3616b0d104fab15253c8
Guan612 <tiantony53@163.com>
2025-07-24 10:46:21 +0800

    新增模型存储

--- src/store/init.tsx
+++ src/store/init.tsx
@@ -1,6 +1,8 @@
 import useApiStore from "./apistore";
+import useModelStore from "./modelstore";
 
 export const initializeAppState = () => {
   console.log("初始化中");
   useApiStore.getState().initializeApi();
+  useModelStore.getState().initializeApi();
 };
--- src/store/modelstore.tsx
+++ src/store/modelstore.tsx
@@ -1,3 +1,36 @@
 import { create } from "zustand";
 import { LazyStore } from "@tauri-apps/plugin-store";
 
+const modelStore = new LazyStore("model.settings.dat");
+
+const useModelStore = create((set) => ({
+  defualtmodel: "",
+  models: [],
+  isLoading: true,
+  initializeApi: async () => {
+    try {
+      const savedApi = await modelStore.get("models");
+      if (savedApi) {
+        set({ model: savedApi });
+      }
+    } catch (error) {
+      console.error("Failed to initialize API store:", error);
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  setModels: async (models: string[]) => {
+    set({ models: models });
+    await modelStore.set("models", models);
+    await modelStore.save;
+  },
+
+  setDefualModel: async (model: string) => {
+    set({ defualtmodel: model });
+    await modelStore.set("defualtmodel", model);
+    await modelStore.save;
+  },
+}));
+
+export default useModelStore;
