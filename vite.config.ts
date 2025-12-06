cf3502e81eef3f66ef2944b63aa819d5407df768
Guan612 <tiantony53@163.com>
2025-12-06 11:57:11 +0800

    配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
